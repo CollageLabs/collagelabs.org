d120722293a43169ca78f97ba8b2024d5f00214c
Paola Vargas <vrgspaola68@gmail.com>
2019-10-08 21:41:41 -0400

    Add sections and styles for home page

--- _src/app.js
+++ _src/app.js
@@ -3,5 +3,6 @@ import * as d3 from 'd3';
 import 'jquery/src/jquery';
 import 'bootstrap';
 import 'cookieconsent';
+import 'slick-carousel';
 import './lib/dom.js';
 import './lib/chart.js';
--- _src/index.js
+++ _src/index.js
@@ -6,3 +6,19 @@ import './app.scss';
 
 // Images
 import '../_images/sprite/brand.svg';
+import '../_images/sprite/Dockershelf.svg';
+import '../_images/sprite/PyPIContents.svg';
+import '../_images/sprite/PipSalaBim.svg';
+import '../_images/sprite/CanaimaKueka.svg';
+
+import '../_images/sprite/instagram.svg';
+import '../_images/sprite/facebook.svg';
+import '../_images/sprite/twitter.svg';
+import '../_images/sprite/icoemail.svg';
+import '../_images/sprite/icophone.svg';
+
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1
+});
