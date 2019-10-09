c553e5756f634e88a4c36e6afb33cd6f3fe25399
Luis Alejandro Martínez Faneyth <luis@luisalejandro.org>
2019-10-08 20:40:03 -0400

    [REF] Fixing jquery import.

--- _src/app.js
+++ _src/app.js
@@ -3,5 +3,6 @@ import * as d3 from 'd3';
 import 'jquery/src/jquery';
 import 'bootstrap';
 import 'cookieconsent';
-import './lib/dom.js';
+import 'slick-carousel';
 import './lib/chart.js';
+import './lib/dom.js';
