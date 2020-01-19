c3508d9587f00716ed0a64c4fff0a7138fa3411d
Luis Alejandro Martínez Faneyth <luis@luisalejandro.org>
2020-01-19 15:41:58 -0400

    [REF] Optimizing js.

--- _src/app.js
+++ _src/app.js
@@ -1,8 +1,9 @@
 // Javascript
-import * as d3 from 'd3';
+// import * as d3 from 'd3';
 import 'jquery/src/jquery';
 import 'bootstrap';
 import 'cookieconsent';
 import 'slick-carousel';
-import './lib/chart.js';
-import './lib/dom.js';
+// import './lib/chart.js';
+import './lib/validate-contact-form';
+import './lib/dom';
