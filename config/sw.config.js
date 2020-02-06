35b6d2d2cba93b4be316aa6fea1e208704a05b5f
Luis Alejandro Martínez Faneyth <luis@luisalejandro.org>
2020-02-05 23:03:28 -0400

    [REF] Changing GAnalytics code, optimizing cache.

--- config/sw.config.js
+++ config/sw.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   staticFileGlobs: [
-    '_site/assets/**.css',
     '_site/**.html',
     '_site/**/**.html',
     '_site/assets/images/**.*',
+    '_site/assets/icons/**.*',
+    '_site/assets/**.css',
     '_site/assets/**.js'
   ],
   stripPrefix: '_site/',
